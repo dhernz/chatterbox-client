21d4d012d38b9431f35adb0f5e2a5fa4cc4ed8d6
dhernz <d.hernz@gmail.com>
2015-07-13 19:03:46 -0700

    Messages showing up on html :!!!

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,22 +1,29 @@
 // YOUR CODE HERE:
 var app = {
-
 	init : function(){
-
+		$(document).ready(function() {
+			// first thing: see previous messages
+			// app.fetch();	
+			app.handleSubmit();
+			$('#send').on('click', function(){
+				//console.log($('#send').text());
+			});
+		});
+		var friendsArray = [];	
 	},
 	send : function(message){
 		$.ajax({
 			type: "POST",
-			// url: url,
+			url: "https://api.parse.com/1/classes/chatterbox",
 			data: JSON.stringify(message)
 			// success: success,
 			// dataType: dataType
 		})
 	},
-
 	fetch: function(){
 		$.ajax({
-			// url: url,
+			type: "GET",
+			url: "https://api.parse.com/1/classes/chatterbox"
 			// data: data,
 			// success: success,
 			// dataType: dataType
@@ -25,15 +32,39 @@ var app = {
 	clearMessages: function(){
 		$('#chats').empty();
 	},
-
 	addMessage: function(message){
-		// var chat = document.getElementById('chats') 
-		// chat.innerHTML = message;
 
-		$("#chats").append("<p>"+message.username+" "+message.text+" "+ message.roomname+"</p>")
+		var obj = {
+			username:"Chris and Doris say:",
+			text: message,
+			roomname:"s"
+		};
+		//currently this adds the message of one user to the page
+		//we want to add an anchor tag to allow the person who sent this 
+		//message to get added to our friends array.
+		$("#chats").append("<p>"+obj.username+" "+ obj.text+"</p>");
+	
 	},
 	addRoom: function(roomname){
-		$("#roomSelect").append('<input type="text" name="'+ roomname +'" />');
-	}
-
+		$("#roomSelect").append('<option value="'+ roomname +'" />');
+	},
+	addFriend: function(username){
+		var btn2 = $(".addFriend")
+		btn2.on('click', function(){
+			//here we will push into the friends array from the anchor in the message.
+			//friendsArray.push()
+		})
+	},
+	handleSubmit: function(){
+		var btn = $("#send")
+		btn.on('click',function(){
+			var msm = $('#message').val();
+			app.addMessage(msm)
+			//console.log(msm);
+		})
+	}	
 };
+
+
+app.init();
+
